00c58600c686f016e03484c4098d2308eb8a6fc6
Peter <xillis@outlook.com>
2020-03-17 14:00:11 +0800

    init vue

---
+++ precache-manifest.70a7d8004f377238ea2246f6a5dc9e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e46d8b02c206c87360",
+    "url": "/css/app.4ece950c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9d1c57bc1ed1f9bd31bb58ad002a0090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb7195ac6382ba42a29f",
+    "url": "/js/about.b663b01f.js"
+  },
+  {
+    "revision": "d3e46d8b02c206c87360",
+    "url": "/js/app.d62458c8.js"
+  },
+  {
+    "revision": "eb5c47a42c44bfee4a6f",
+    "url": "/js/chunk-vendors.816d8bd0.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/manifest.json"
+  }
+]);
